07a6029a97a6a2c75ccd6642655e795f82ca926f
Dani <hathazi.daniel@gmail.com>
2022-05-18 18:49:55 +0200

    97% coverage #4

--- src/main/java/complexible/common/csv/CSV2RDF.java
+++ src/main/java/complexible/common/csv/CSV2RDF.java
@@ -1,47 +1,8 @@
 // Copyright (c) 2014, Clark & Parsia, LLC. <http://www.clarkparsia.com>
 
-package com.complexible.common.csv;
-
-import io.airlift.command.Arguments;
-import io.airlift.command.Cli;
-import io.airlift.command.Command;
-import io.airlift.command.Help;
-import io.airlift.command.Option;
-
-import java.io.File;
-import java.io.Reader;
-import java.io.StringReader;
-import java.io.Writer;
-import java.nio.charset.Charset;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.UUID;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import org.openrdf.model.BNode;
-import org.openrdf.model.Literal;
-import org.openrdf.model.Resource;
-import org.openrdf.model.Statement;
-import org.openrdf.model.URI;
-import org.openrdf.model.Value;
-import org.openrdf.model.ValueFactory;
-import org.openrdf.model.impl.ValueFactoryImpl;
-import org.openrdf.rio.ParserConfig;
-import org.openrdf.rio.RDFFormat;
-import org.openrdf.rio.RDFHandler;
-import org.openrdf.rio.RDFHandlerException;
-import org.openrdf.rio.RDFParser;
-import org.openrdf.rio.RDFWriter;
-import org.openrdf.rio.Rio;
-import org.openrdf.rio.RioSetting;
-import org.openrdf.rio.helpers.BasicParserSettings;
-import org.openrdf.rio.helpers.RDFHandlerBase;
+package complexible.common.csv;
 
 import au.com.bytecode.opencsv.CSVReader;
-
 import com.google.common.base.Charsets;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.Lists;
@@ -51,6 +12,21 @@ import com.google.common.hash.HashCode;
 import com.google.common.hash.Hashing;
 import com.google.common.io.BaseEncoding;
 import com.google.common.io.Files;
+import io.airlift.command.*;
+import org.openrdf.model.*;
+import org.openrdf.model.impl.ValueFactoryImpl;
+import org.openrdf.rio.*;
+import org.openrdf.rio.helpers.BasicParserSettings;
+import org.openrdf.rio.helpers.RDFHandlerBase;
+
+import java.io.File;
+import java.io.Reader;
+import java.io.StringReader;
+import java.io.Writer;
+import java.nio.charset.Charset;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Converts a CSV file to RDF based on a given template
@@ -81,6 +57,8 @@ public class CSV2RDF implements Runnable {
 	private int inputRows = 0;
 	private int outputTriples = 0;
 
+	public RDFWriter writer;
+
 	public void run() {
 		Preconditions.checkArgument(files.size() >= 3, "Missing arguments");
 		Preconditions.checkArgument(files.size() <= 3, "Too many arguments");
@@ -101,7 +79,7 @@ public class CSV2RDF implements Runnable {
 			Preconditions.checkNotNull(row, "Input file is empty!");
 
 			Writer out = Files.newWriter(outputFile, OUTPUT_CHARSET);
-			RDFWriter writer = Rio.createWriter(RDFFormat.forFileName(outputFile.getName(), RDFFormat.TURTLE), out);
+			writer = Rio.createWriter(RDFFormat.forFileName(outputFile.getName(), RDFFormat.TURTLE), out);
 
 			Template template = new Template(Arrays.asList(row), templateFile, writer);
 
